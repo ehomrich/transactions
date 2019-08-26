287725fc316bc3faaad381c83131b60053f34a45
Émerson P. Homrich <ehomrich@users.noreply.github.com>
2019-08-25 22:59:23 -0300

    Decode input

--- src/transactions/core.clj
+++ src/transactions/core.clj
@@ -1,10 +1,13 @@
 (ns transactions.core
+  (:require [cheshire.core :refer [decode]])
   (:gen-class))
+
+(def account (atom nil))
 
 (defn -main
   "Read from sdtin."
   [& args]
   (loop [line (read-line)]
     (when line
-      (println line)
+      (println (decode line true))
       (recur (read-line)))))
