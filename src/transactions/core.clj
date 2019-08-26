23fabb861249f7289506eab74dc7d5e3d25c3997
Émerson P. Homrich <ehomrich@users.noreply.github.com>
2019-08-26 00:45:18 -0300

    Define schemas and a basic test function
    - Defines schemas for account and transaction using draft-07
    specification.
    - Creates a predicate method to validate the input. Since json-schema lib
    throws instances of ExceptionInfo when validation fails, the method
    suppresses errors and returns truthy or falsy values.

--- src/transactions/core.clj
+++ src/transactions/core.clj
@@ -1,8 +1,33 @@
 (ns transactions.core
-  (:require [cheshire.core :refer [decode]])
+  (:require [cheshire.core :refer [decode]]
+            [json-schema.core :as json])
   (:gen-class))
 
 (def account (atom nil))
+
+(def account-schema {:$schema "http://json-schema.org/draft-07/schema#"
+                     :id "https://example.com/account-schema.json"
+                     :type "object"
+                     :properties {:account {:type "object"
+                                            :properties {:activeCard {:type "boolean"}
+                                                         :availableLimit {:type "number"}}
+                                            :required [:activeCard :availableLimit]}}
+                     :required [:account]})
+
+(def transaction-schema {:$schema "http://json-schema.org/draft-07/schema#"
+                         :id "https://example.com/transaction-schema.json"
+                         :type "object"
+                         :properties {:transaction {:type "object"
+                                                    :properties {:merchant {:type "string"}
+                                                                 :amount {:type "number"}
+                                                                 :time {:type "string"}}
+                                                    :required [:merchant :amount :time]}}
+                         :required [:transaction]})
+
+(defn valid-input? [schema, data]
+  (try
+    (json/validate schema data)
+    (catch clojure.lang.ExceptionInfo ex false)))
 
 (defn -main
   "Read from sdtin."
@@ -10,7 +35,9 @@
   (loop [line (read-line)]
     (when line
       (try
-        (println (decode line true))
+        (let [input (decode line true)]
+          (println (valid-input? account-schema input))
+          (println (valid-input? transaction-schema input)))
         ;; HACK: "catch all" block because `cheshire` does not explicitly list 
         ;; exceptions that may occur.
         (catch Exception ex
