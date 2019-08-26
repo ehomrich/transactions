010c2bdeedd7dc12afe4b483e7a321d86ca3af71
Émerson P. Homrich <ehomrich@users.noreply.github.com>
2019-08-26 00:45:09 -0300

    Add JSON schema validator to dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [cheshire "5.9.0"]]
+                 [cheshire "5.9.0"]
+                 [luposlip/json-schema "0.1.7"]]
   :main ^:skip-aot transactions.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
