8f96b6321ac780ee196f1740c31ec9c9dc87194e
Émerson P. Homrich <ehomrich@users.noreply.github.com>
2019-08-26 03:08:15 -0300

    Add bangs to mutation methods

--- src/transactions/db/account.clj
+++ src/transactions/db/account.clj
@@ -2,10 +2,10 @@
 
 (def account (atom {}))
 
-(defn create-account [account data]
+(defn create-account! [account data]
   (reset! account data))
 
-(defn update-account [account data]
+(defn update-account! [account data]
   (swap! account merge data))
 
 (defn get-account [] @account)
