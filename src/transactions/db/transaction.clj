e843c55a58118fc215f77116e7b31c02a3b75bfe
Émerson P. Homrich <ehomrich@users.noreply.github.com>
2019-08-27 01:20:08 -0300

    Fix name clashes

--- src/transactions/db/transaction.clj
+++ src/transactions/db/transaction.clj
@@ -2,7 +2,7 @@
 
 (def tx-storage (atom []))
 
-(defn save-transaction! [storage tx]
-  (swap! storage conj tx))
+(defn save-transaction! [tx]
+  (swap! tx-storage conj tx))
 
 (defn get-transaction-history [] @tx-storage)
