565969156a6dda86b48f836229714da4e409e46c
Émerson P. Homrich <ehomrich@users.noreply.github.com>
2019-08-29 02:43:59 -0300

    Add more controller tests

--- test/transactions/controller_test.clj
+++ test/transactions/controller_test.clj
@@ -3,8 +3,8 @@
             [transactions.controller :as ctrl]))
 
 (fact "Prepare output"
-      (ctrl/prepare-output {:foo true} ["card-blocked"]) 
-      => (contains {:accounts {:foo true} :violations ["card-blocked"]}))
+      (ctrl/prepare-output {:foo true} ["card-blocked"])
+      => (contains {:account {:foo true} :violations ["card-blocked"]}))
 
 (facts "Violation checks"
        (fact "Unknown account"
@@ -12,7 +12,13 @@
 
        (fact "Card blocked"
              (ctrl/check-violations {:activeCard false} nil nil)
-             => "card-blocked"))
+             => "card-blocked")
+
+       (fact "Insufficient limit"
+             (ctrl/check-violations {:activeCard true :availableLimit 30}
+                                    {:amount 50}
+                                    nil)
+             => "insufficient-limit"))
 
 (facts "Validate operation"
        (fact "Unrecognized input"
