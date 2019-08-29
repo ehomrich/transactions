1f9efb6e218c10eb713cb2f1bff75fe6cdbd2a8b
Émerson P. Homrich <ehomrich@users.noreply.github.com>
2019-08-29 02:38:42 -0300

    Add basic controller tests

--- test/transactions/controller_test.clj
+++ test/transactions/controller_test.clj
@@ -1,6 +1,24 @@
 (ns transactions.controller-test
   (:require [midje.sweet :refer :all]
-            [transactions.controller :as controller]))
+            [transactions.controller :as ctrl]))
 
-(fact
- (some even? [1 5]) => falsey)
+(fact "Prepare output"
+      (ctrl/prepare-output {:foo true} ["card-blocked"]) 
+      => (contains {:accounts {:foo true} :violations ["card-blocked"]}))
+
+(facts "Violation checks"
+       (fact "Unknown account"
+             (ctrl/check-violations {} nil nil) => "unknown-account")
+
+       (fact "Card blocked"
+             (ctrl/check-violations {:activeCard false} nil nil)
+             => "card-blocked"))
+
+(facts "Validate operation"
+       (fact "Unrecognized input"
+             (ctrl/validate-operation {})
+             => (throws Exception)))
+
+(facts "Process data"
+       (fact "Input parsing error"
+             (ctrl/process-data "{") => ""))
